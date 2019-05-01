5b015f688c6593d649140fe9df71e849f1607fea
alanreji <alanreji.pulickal@gmail.com>
2019-05-01 17:43:09 +0530

    index and item page works

--- app.py
+++ app.py
@@ -1,41 +1,57 @@
 from flask import Flask
 from flask import render_template
-from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    render_template('index-2.html')
+    return render_template('index.html')
+
 
 @app.route("/orders")
 def orders():
     return "order"
 
+
 @app.route("/account")
 def account():
     return "account"
+
+
+@app.route("/logout")
+def logout():
+    return "logout"
+
 
 @app.route("/checkout")
 def checkout():
     return "Checkout"
 
+
 @app.route("/checkout_confirm")
 def checkout_confirm():
     return "checkout_confirm"
+
 
 @app.route("/invoice")
 def invoice():
     return "invoice"
 
+
 @app.route("/listing")
 def listing():
     return "listing"
 
+
 @app.route("/item")
 def item():
-    return "item"
+    return render_template("item.html")
+
+
+@app.route("/contact")
+def contact():
+    return "contact"
 
 
 if __name__ == '__main__':
